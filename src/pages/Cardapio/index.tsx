44386b21ac483d5c1140ad79fd5a1f4cb91a86a8
Gabriela Florêncio <perflogabi@gmail.com>
2022-11-15 20:55:29 -0300

    quarto commit

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,11 @@
 import styles from './Cardapio.module.scss';
-import {ReactComponent as Logo} from "";
+import {ReactComponent as Logo} from "assets/cardapio/logo.svg";
 
 export default function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-        <img src="assets/logo.svg" alt="logo da esfirrou" />
+        <img src={Logo} alt="logo da esfirrou" />
       </nav>
     </main>
   )
